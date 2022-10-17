2a5e170ba9c677481949570178b5003c227b1c92
thelosercom <paul.b.michaelis@icloud.com>
2022-10-16 17:56:08 -0700

    Update data.py

--- data.py
+++ data.py
@@ -1,4 +1,4 @@
-lst = [
+lst = [https://ridefiles.net/show.php?l=0&u=847000&id=39509&tracking_id=]
     ["https://zoom.us/j/98105909103", "8:30"],["https://zoom.us/j/95951255535", "9:45"], ["https://zoom.us/j/6525592393", "11:30"],  ["https://zoom.us/j/6525592393", "14:00"], ["", "15:15"]
 ]
 # This is an Example
